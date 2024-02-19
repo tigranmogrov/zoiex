ab524ff7ccc5dbf2c983ad8adae73ddacc41d074
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 17:27:23 +0200

    feat: add GiphyView trendingPost carousel

--- src/views/GiphyView.vue
+++ src/views/GiphyView.vue
@@ -7,8 +7,9 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 const postStore = usePostStore();
 const isLoaded = ref(true);
-const { currentPost } = storeToRefs(postStore);
+const { currentPost, trendingPosts } = storeToRefs(postStore);
 postStore.getPostById(String(route.params.id)).finally(() => (isLoaded.value = false));
+postStore.getTrendingPosts();
 </script>
 
 <template>
@@ -36,6 +37,24 @@ postStore.getPostById(String(route.params.id)).finally(() => (isLoaded.value = f
             :lazy-src="`https://picsum.photos/10/6?image=${3 * 5 + 10}`"
             :alt="currentPost.title || currentPost.slug"
           />
+        </v-col>
+        <v-col cols="12" md="7">
+          <v-slide-group class="py-4 py-md-0" show-arrows>
+            <v-slide-group-item v-for="post in trendingPosts" :key="post.id">
+              <v-card class="ma-2 ma-md-4" height="170" width="100">
+                <v-img
+                  aspect-ratio="1"
+                  cover
+                  width="100%"
+                  height="100%"
+                  class="bg-grey-lighten-2"
+                  :src="post.images.fixed_height.url"
+                  :lazy-src="`https://picsum.photos/10/6?image=${3 * 5 + 10}`"
+                  :alt="post.title || post.slug"
+                />
+              </v-card>
+            </v-slide-group-item>
+          </v-slide-group>
         </v-col>
       </v-row>
     </div>
