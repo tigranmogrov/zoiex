93a0dd53a17c15d017e07c73e907ec909a360fe2
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 15:25:22 +0200

    feat(layouts): create empty layout and default

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <app-header :is-search-visible="false" />
+  <v-layout>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-layout>
+</template>
+
+<style scoped></style>
