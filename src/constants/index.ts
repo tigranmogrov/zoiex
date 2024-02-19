5826503e5b0d416778a77d263c499de89f3af4a5
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 16:39:43 +0200

    feat(constants): add default search value variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 // Ideally, the key should be stored in a file .env
 export const API_KEY = 'Byt3WMl3PLoWkTkkUZ9P5zsV5kTOWRpr';
 export const API_URL = `https://api.giphy.com/v1/gifs`;
+export const DEFAULT_SEARCH_VALUE = 'garry';
