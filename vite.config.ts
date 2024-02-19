e8a1e8ee81cb62274651ea6d46be18027a4eeb31
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 14:48:10 +0200

    feat: add vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,56 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      base: '/zoinex/',
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globDirectory: 'dist/',
+        swDest: 'dist/sw.js',
+        globPatterns: [
+          '**/*.{js, css, html, png, jpg, jpeg, svg, woff2, woff, ttf, oet, ico, txt, vue}'
+        ]
+      },
+      manifest: {
+        name: 'Zoinex App',
+        short_name: 'Zoinex',
+        start_url: '.',
+        display: 'fullscreen',
+        background_color: '#fff',
+        description: 'Zoinex test project',
+        icons: [
+          {
+            src: 'img/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: process.env.NODE_ENV === 'development' ? '/' : '/zoinex/',
   resolve: {
     alias: {
