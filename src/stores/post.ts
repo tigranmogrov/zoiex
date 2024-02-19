ca27f367bd68b603380d8d44adf365c1c3d62fef
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 16:26:48 +0200

    feat(store): add action fetchPosts and getters allPosts

--- src/stores/post.ts
+++ src/stores/post.ts
@@ -1,4 +1,7 @@
-import type { IPostStore } from '@/types/post.types';
+import { API } from '@/api/axios';
+import { DEFAULT_SEARCH_VALUE } from '@/constants';
+import type { IPost, IPostData, IPostStore } from '@/types/post.types';
+import { randomNumber } from '@/utils';
 import { defineStore } from 'pinia';
 
 export const usePostStore = defineStore('post', {
@@ -17,5 +20,31 @@ export const usePostStore = defineStore('post', {
         currentPage: 0
       }
     };
+  },
+  getters: {
+    allPosts: (state): IPostData[] => state.posts
+  },
+  actions: {
+    async fetchPosts(searchString?: string, offset: number = 0): Promise<void> {
+      try {
+        const {
+          data: { data, pagination }
+        }: { data: IPost } = await API.get('/search', {
+          params: { q: searchString || DEFAULT_SEARCH_VALUE, limit: this.pagination.limit, offset }
+        });
+        this.isNotFoundPosts = false;
+        this.posts = data;
+        this.pagination.totalCount = pagination['total_count'];
+        this.pagination.currentPage = 0;
+        this.pagination.currentOffset = 0;
+        if (pagination['total_count'] === 0) {
+          this.isNotFoundPosts = true;
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.isLoading = false;
+      }
+    }
   }
 });
