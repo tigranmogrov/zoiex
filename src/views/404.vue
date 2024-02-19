ae351b7cb8a4df9cc38e1d2f5effd16307efbf9a
Tigran Mogrov <tigranmogrov@gmail.com>
2024-02-19 15:14:28 +0200

    feat: create empty layouts

---
+++ src/views/404.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="v-row align-center justify-center">
+    <div class="v-col v-col-auto">
+      <h1 class="text-h3 text-primary">Whoops, 404</h1>
+      <p>The page you were looking for does not exist</p>
+      <router-link
+        class="v-btn v-btn--variant-outlined bg-blue-accent-3 pa-4 d-flex ma-auto mt-4 w-75 text-white"
+        to="/"
+        >Back Home</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
